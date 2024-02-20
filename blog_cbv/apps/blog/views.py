41b1835ad03921b1cccd78866dffa32c6fa36e21
Dmitry Terebilko <dimaterebilko75@gmail.com>
2024-02-20 20:10:01 +0300

    work with Form Category

--- blog_cbv/apps/blog/views.py
+++ blog_cbv/apps/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Post
+from .models import Post, Category
 
 class PostListView(ListView):
     model = Post
@@ -20,5 +20,24 @@ class PostDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = self.object.title
+        return context
+
+class PostFromCategory(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    category = None
+
+    def get_queryset(self):
+        self.category = Category.objects.get(slug=self.kwargs['slug'])
+        queryset = Post.objects.filter(category__slug=self.category.slug)
+        if not queryset:
+            sub_cat = Category.objects.filter(parent=self.category)
+            queryset = Post.objects.filter(category__in=sub_cat)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = f"Записи категории: {self.category.title}"
         return context
 
