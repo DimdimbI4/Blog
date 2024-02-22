325144554f87d2f48072c538048a2bc1142e4f07
Dmitry Terebilko <dimaterebilko75@gmail.com>
2024-02-22 18:10:43 +0300

    comments AJAX

--- blog_cbv/apps/blog/admin.py
+++ blog_cbv/apps/blog/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Post, Category
 from django_mptt_admin.admin import DjangoMpttAdmin
+from .models import Comment
+
+@admin.register(Comment)
+class CommentAdminPage(DjangoMpttAdmin):
+    """
+    Админ панель модели комментариев
+    """
+    pass
 
 @admin.register(Category)
 class CategoryAdmin(DjangoMpttAdmin):
