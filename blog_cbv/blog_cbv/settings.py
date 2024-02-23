997e46f078943d40e68b553d60be51dd9f192e06
Dmitry Terebilko <dimaterebilko75@gmail.com>
2024-02-23 20:26:21 +0300

    debug false for settings and view errors

--- blog_cbv/blog_cbv/settings.py
+++ blog_cbv/blog_cbv/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tpb(v3d0346a-bn23gdq4gwl_0c=_$-j@(!ii)(iaj%+dov)a$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 INTERNAL_IPS = [
     '127.0.0.1',
