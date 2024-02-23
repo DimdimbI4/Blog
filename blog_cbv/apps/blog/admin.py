3ec13c8db52959226b812e8bb7f5dfab7e028613
Dmitry Terebilko <dimaterebilko75@gmail.com>
2024-02-23 19:58:03 +0300

    add system like/dislike

--- blog_cbv/apps/blog/admin.py
+++ blog_cbv/apps/blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Post, Category
 from django_mptt_admin.admin import DjangoMpttAdmin
-from .models import Comment
+from .models import Comment, Rating
 
 @admin.register(Comment)
 class CommentAdminPage(DjangoMpttAdmin):
@@ -17,3 +17,10 @@ class CategoryAdmin(DjangoMpttAdmin):
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title', )}
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    """
+    Админ панель модели рейтинга
+    """
+    pass
