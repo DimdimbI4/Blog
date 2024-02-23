b68f3e6c2bf2a070af0bf048ef846d96ac22e0b0
Dmitry Terebilko <dimaterebilko75@gmail.com>
2024-02-23 20:09:25 +0300

    RSS

--- blog_cbv/blog_cbv/urls.py
+++ blog_cbv/blog_cbv/urls.py
@@ -18,12 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from apps.blog.feeds import LatestPostFeed
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.blog.urls')),
     path('', include('apps.accounts.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('feeds/latest/', LatestPostFeed(), name='latest_post_feed'),
 ]
 
 if settings.DEBUG:
